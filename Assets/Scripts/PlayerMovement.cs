14d61d245d50b799c458a480b1d097ec39c3c740
Dev <d.trivedi@saedubai.com>
2021-10-20 18:37:48 +0400

    Island Escape (0.3)
    - Changed up the movement script to add a Launch function along with a Timer
    - Fixed the incorrect tag instance
    - Set the player to be on the ground without it colliding under

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,61 +4,88 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector3 targetPosition;
-    Vector3 lookAtTarget;
-    public bool playerClicked;
-    Quaternion playerRot;
-    public int rotSpeed;
-    public int moveSpeed;
-    public Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
-    {
+	Vector3 targetPosition;
+	Vector3 lookAtTarget;
+	public bool playerClicked;
+	Quaternion playerRot;
+	public int rotSpeed;
+	public int moveSpeed;
+	public Vector3 offset;
+	public float speedforce = 100f;
+	Rigidbody rb;
+	float holdStartTime;
+    Transform forceTransform;
 
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            playerClicked = true;
-            SetTargetPosition();
-        }
-       // if (Input.GetMouseButtonUp(0))
-       // {
-       //     playerClicked = false;
-        //}
-     
-    }
-    private void FixedUpdate()
-    {
-        if (playerClicked)
-        {
-            Move();
-        }
-       
-    }
-    public void SetTargetPosition()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+			playerClicked = true;
+			SetTargetPosition();
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			playerClicked = true;
+			SetTargetPosition();
+			float holdDownTime = Time.time - holdStartTime;
+			Launch(HoldDownForce(holdDownTime));
+		}
+		// if (Input.GetMouseButtonUp(0))
+		// {
+		//     playerClicked = false;
+		//}
 
-        if(Physics.Raycast(ray, out hit, 1000))
-        {
-            targetPosition = hit.point;
-            Debug.Log(targetPosition);
-            //this.transform.LookAt(targetPosition);
-            lookAtTarget = new Vector3(targetPosition.x - transform.position.x,targetPosition.y - transform.position.y, 
-                targetPosition.z - transform.position.z);
-            playerRot = Quaternion.LookRotation(lookAtTarget);
+	}
+	private void Launch(float force)
+	{
+        rb.position = transform.position;
+        Vector3 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized * 1f;
+        rb.velocity = direction * force;
+	}
+	private float HoldDownForce(float holdTime)
+	{
+		float maxForceHoldTime = 3f;
+		float holdTimeNormalized = Mathf.Clamp01(holdTime / maxForceHoldTime);
+		float force = holdTimeNormalized * speedforce;
+        return force;
+	}
 
+	private void FixedUpdate()
+	{
+		if (playerClicked)
+		{
+			Move();
+			playerClicked = false;
+		}
 
-        }
-    }
-    public void Move()
-    {
-        transform.rotation = Quaternion.Slerp(transform.rotation, playerRot, rotSpeed * Time.deltaTime);
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition + offset, moveSpeed * Time.deltaTime);
-    }
+	}
+	public void SetTargetPosition()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, 1000))
+		{
+			targetPosition = hit.point;
+			Debug.Log(targetPosition);
+			//this.transform.LookAt(targetPosition);
+			lookAtTarget = new Vector3(targetPosition.x - transform.position.x, targetPosition.y - transform.position.y,
+				targetPosition.z - transform.position.z);
+			playerRot = Quaternion.LookRotation(lookAtTarget);
+
+		}
+	}
+	public void Move()
+	{
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, playerRot, rotSpeed * Time.deltaTime);
+		transform.position = Vector3.MoveTowards(transform.position, targetPosition + offset, moveSpeed * Time.deltaTime);
+	}
 }
