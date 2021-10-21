e38ef7093374e32b80ea35aec2bab50ffbd6a6e1
Dev <d.trivedi@saedubai.com>
2021-10-21 11:39:14 +0400

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/PlayerDash.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+	float dashTime;
+	public float startDashTime;
+	float direction;
+	Rigidbody rb;
+    Movement playerPosition;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		dashTime = startDashTime;
+	}
+
+	void Update()
+	{
+		if (direction == 0)
+		{
+			if (Input.GetMouseButtonUp(0))
+			{
+                
+			}
+		}
+	}
+}
