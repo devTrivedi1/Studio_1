47f1cf10180a64a88a443a5d4af507e0d8ece4d6
Dev <d.trivedi@saedubai.com>
2021-11-15 11:26:40 +0400

    Island Escape 1.1.1
    - Just minor tweaks, changes to level 1 and 2
    - Moved the guardRoation to its appropriate place
    - Deleted a guard script that wasn't need (something guard shooting long range script something)

--- Assets/Scripts/Guard Scripts/GuardShoot.cs
+++ Assets/Scripts/Guard Scripts/GuardShoot.cs
@@ -4,72 +4,66 @@ using UnityEngine;
 
 public class GuardShoot : MonoBehaviour
 {
-    public GameObject theProjectile;
-    private int startSpawn;
-    public float spawnDelay;
+	public GameObject theProjectile;
+	private int startSpawn;
+	public float spawnDelay;
 
-    private Vector3 spawnOffset;
+	private Vector3 spawnOffset;
 
-    public float shootingRange = 40f;
-    public float distanceToPlayer;
-    private GameObject thePlayer;
-    public bool inShootingRange;
+	public float shootingRange = 40f;
+	public float distanceToPlayer;
+	private GameObject thePlayer;
+	public bool inShootingRange;
 
-    public float timer;
+	public float timer;
 
-    public float projectileMoveSpeed;
+	public float projectileMoveSpeed;
+	void Start()
+	{
+		startSpawn = 1;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        startSpawn = 1;
+		spawnOffset = new Vector3(0, 0, 2);
+		thePlayer = GameObject.FindGameObjectWithTag("Player");
+	}
 
-        spawnOffset = new Vector3(0, 0, 2);
-        thePlayer = GameObject.FindGameObjectWithTag("Player");
-    }
+	
+	void Update()
+	{
+		spawnDelay = 1;
+		DetectPlayer();
 
-    // Update is called once per frame
-    void Update()
-    {
-        spawnDelay = 1;
-        DetectPlayer();
+		timer += Time.deltaTime;
+	}
 
-        timer += Time.deltaTime;
+	private void DetectPlayer()
+	{
+		distanceToPlayer = Vector3.Distance(thePlayer.transform.position, transform.position);
+		if (distanceToPlayer <= shootingRange)
+		{
+			inShootingRange = true;
+			Debug.Log("Player is in range");
+			if (timer >= spawnDelay)
+			{
+				StartFiring();
+			}
 
+		}
+		else
+		{
+			inShootingRange = false;
+		}
+	}
+    
+	public void StartFiring()
+	{
+		timer = 0;
+		GameObject projectile = Instantiate(theProjectile, transform.position + spawnOffset, transform.rotation);
+		Rigidbody rb = projectile.GetComponent<Rigidbody>();
+		Vector3 shootForce = Vector3.forward * projectileMoveSpeed;
+		rb.AddForce(transform.TransformDirection(shootForce), ForceMode.Impulse);
 
-    }
-
-
-
-
-    private void DetectPlayer()
-    {
-        distanceToPlayer = Vector3.Distance(thePlayer.transform.position, transform.position);
-        if (distanceToPlayer <= shootingRange)
-        {
-            inShootingRange = true;
-            Debug.Log("Player is in range");
-            if (timer >= spawnDelay)
-            {
-                StartFiring();
-            }
-
-        }
-        else
-        {
-            inShootingRange = false;
-        }
-    }
-    public void StartFiring()
-    {
-        timer = 0;
-        GameObject projectile = Instantiate(theProjectile, transform.position + spawnOffset, transform.rotation);
-        Rigidbody rb = projectile.GetComponent<Rigidbody>();
-        Vector3 shootForce = Vector3.forward * projectileMoveSpeed;
-        rb.AddForce(transform.TransformDirection(shootForce), ForceMode.Impulse);
-
-        Rigidbody rbGuard = this.transform.GetComponent<Rigidbody>();
-        rbGuard.freezeRotation = true;
-    }
+		Rigidbody rbGuard = this.transform.GetComponent<Rigidbody>();
+		rbGuard.freezeRotation = true;
+	}
 
 }
