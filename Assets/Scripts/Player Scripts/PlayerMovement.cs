1b6bd85807d5a4d7e0281524287da2043f6b24b9
Hasan Ahmed Abdalla <hasanhasan.molair@gmail.com>
2021-11-26 13:47:04 +0400

    Fixed the player lunging issue where it would slowly float downwards. Made it so trap panels will despawn after being stepped on once.

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,88 +4,122 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public Vector3 targetPosition;
-	public Vector3 lookAtTarget;
-	public bool playerClicked;
-	Quaternion playerRot;
-	public float moveSpeed;
-	private Vector3 offset = new Vector3(0, 1.5f, 0);
+    public Vector3 targetPosition;
+    public Vector3 lookAtTarget;
+    public bool playerClicked;
+    Quaternion playerRot;
+    public float moveSpeed;
+    private Vector3 offset = new Vector3(0, 1.5f, 0);
 
-	public Vector3 newPosition;
-	public LayerMask layerMask;
+    public Vector3 newPosition;
+    public LayerMask layerMask;
 
-	private Rigidbody rb;
+    private Rigidbody rb;
 
-	public PlayerLauncher thePlayer;
-	//Transform forceTransform;
+    public PlayerLauncher playerLauncher;
+    public PlayerLunge playerLunge;
+    //Transform forceTransform;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		moveSpeed = 150f;
-		rb = GetComponent<Rigidbody>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 150f;
+        rb = GetComponent<Rigidbody>();
 
-		thePlayer = GetComponent<PlayerLauncher>();
-	}
+        playerLauncher = GetComponent<PlayerLauncher>();
+        playerLunge = GetComponent<PlayerLunge>();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
+    // Update is called once per frame
+    void Update()
+    {
 
-		if (Input.GetMouseButtonUp(0))
-		{
-			playerClicked = true;
-			SetTargetPosition();
-		}
-	}
-
-
-	private void FixedUpdate()
-	{
-		if (playerClicked)
-		{
-			Move();
-			/* ReleaseDamage(); */
-		}
-
-	}
-	public void SetTargetPosition()
-	{
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-
-		if (Physics.Raycast(ray, out hit, 1000, layerMask))
-		{
-			targetPosition = hit.point;
-			//Debug.Log("target pos is " + targetPosition);
-			//this.transform.LookAt(targetPosition);
-			lookAtTarget = new Vector3(targetPosition.x - transform.position.x, targetPosition.y - transform.position.y,
-				targetPosition.z - transform.position.z) * Time.fixedDeltaTime;
-			playerRot = Quaternion.LookRotation(lookAtTarget);
-
-		}
-
-	}
+        if (Input.GetMouseButtonUp(0))
+        {
+            playerClicked = true;
+            SetTargetPosition();
+        }
+        Debug.Log(rb.velocity);
+    }
 
 
-	public void Move()
-	{
+    private void FixedUpdate()
+    {
+        if (playerClicked)
+        {
+            Move();
+            /* ReleaseDamage(); */
+        }
 
-		/* rb.velocity = (targetPosition - this.transform.position) * moveSpeed * Time.fixedDeltaTime; */
-		Vector3 direction = new Vector3(targetPosition.x - transform.position.x, 0, targetPosition.z - transform.position.z);
-		rb.velocity = direction * thePlayer.currentSpeedBoost;
-		if (transform.position == targetPosition)
-		{
-			rb.velocity = Vector3.zero;
-		}
+    }
+    public void SetTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-		/* rb.velocity = Vector3.MoveTowards(this.transform.position, targetPosition + offset, moveSpeed * Time.deltaTime); */
-		/* transform.rotation = Quaternion.Slerp(transform.rotation, playerRot, moveSpeed * Time.deltaTime); */
-		//newPosition = Vector3.MoveTowards(transform.position, targetPosition + offset, moveSpeed * Time.deltaTime);
-		//transform.position = newPosition;
-	}
-	public void ReleaseDamage()
-	{
-		rb.AddExplosionForce(10, new Vector3(10, 10, 10), 10, 1, ForceMode.Impulse);
-	}
+        if (Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            targetPosition = hit.point;
+            //Debug.Log("target pos is " + targetPosition);
+            //this.transform.LookAt(targetPosition);
+            lookAtTarget = new Vector3(targetPosition.x - transform.position.x, targetPosition.y - transform.position.y,
+                targetPosition.z - transform.position.z) * Time.fixedDeltaTime;
+            playerRot = Quaternion.LookRotation(lookAtTarget);
+
+        }
+
+    }
+
+
+    public void Move()
+    {
+
+        /* rb.velocity = (targetPosition - this.transform.position) * moveSpeed * Time.fixedDeltaTime; */
+
+        if (playerLunge.isGrounded == false)
+        {
+            Vector3 direction = new Vector3(targetPosition.x - transform.position.x, targetPosition.y - transform.position.y, targetPosition.z - transform.position.z);
+            rb.velocity = direction * playerLauncher.currentSpeedBoost;
+        }
+        else
+        {
+            Vector3 direction = new Vector3(targetPosition.x - transform.position.x, 0, targetPosition.z - transform.position.z);
+            rb.velocity = direction * playerLauncher.currentSpeedBoost;
+        }
+
+
+        if (transform.position == targetPosition)
+        {
+            rb.velocity = Vector3.zero;
+            Debug.Log(rb.velocity);
+        }
+
+        /* rb.velocity = Vector3.MoveTowards(this.transform.position, targetPosition + offset, moveSpeed * Time.deltaTime); */
+        /* transform.rotation = Quaternion.Slerp(transform.rotation, playerRot, moveSpeed * Time.deltaTime); */
+        //newPosition = Vector3.MoveTowards(transform.position, targetPosition + offset, moveSpeed * Time.deltaTime);
+        //transform.position = newPosition;
+    }
+    public void ReleaseDamage()
+    {
+        rb.AddExplosionForce(10, new Vector3(10, 10, 10), 10, 1, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            rb.velocity = Vector3.zero;
+        }
+        if(collision.gameObject.tag == "TrapPanel")
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        }
+    }
+  /*  private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "TrapPanel")
+        {
+            Destroy(collision.gameObject);
+        }
+    }*/
 }
