58e32cddd6e335e16e153c8572538c1f75e5a2bc
Dev <d.trivedi@saedubai.com>
2021-12-12 13:10:18 +0400

    Animation script works now yay

--- Assets/Scripts/Guard Scripts/PatrollerGuard/GuardEnemySpawner.cs
+++ Assets/Scripts/Guard Scripts/PatrollerGuard/GuardEnemySpawner.cs
@@ -11,72 +11,37 @@ public class GuardEnemySpawner : MonoBehaviour
 	private float timer;
 
 	public int currentSpawnCount;
-	 public int maxSpawnCount;
-    public int currentSpawnCount;
-    public int maxSpawnCount;
+	public int maxSpawnCount;
 
-	// Start is called before the first frame update
+
 	void Start()
 	{
 		pos = new Vector3(Random.Range(-10, 10), 1.4f, Random.Range(-10, 10));
+		currentSpawnCount = 0;
 	}
-    /* public int currentSpawnCount;
-     public int maxSpawnCount;*/
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        pos = new Vector3(Random.Range(-10, 10), 1.4f, Random.Range(-10, 10));
-        currentSpawnCount = 0;
-    }
 
 	// Update is called once per frame
 	void Update()
 	{
 		spawnDelay = 1;
 		timer += Time.deltaTime;
-        LimitSpawning();
+		LimitSpawning();
 	}
 	public void SpawnEnemies()
 	{
 		if (timer > spawnDelay)
 		{
 			Instantiate(spawnEnemies, transform.TransformPoint(pos), transform.rotation);
-            currentSpawnCount++;
+			currentSpawnCount++;
 			timer = 0;
 		}
 	}
-    private void LimitSpawning()
-    {
-        spawnDelay = 1;
-        timer += Time.deltaTime;
-        LimitSpawning();
-    }
-    public void SpawnEnemies()
-    {
 
-        if (timer > spawnDelay)
-        {
-            Instantiate(spawnEnemies, transform.TransformPoint(pos), transform.rotation);
-            currentSpawnCount++;
-            timer = 0;
-        }
-
-    }
-    /*public void LimitSpawning()
-    {
-        GameObject[] shooterGuards = GameObject.FindGameObjectsWithTag("ShooterSpawnee");
-        if (shooterGuards.Length > 3)
-        {
-            spawnEnemies.SetActive(false);
-            Debug.Log("too many guards");
-        }
-    }*/
-    public void LimitSpawning()
-    {
-        if (currentSpawnCount >= maxSpawnCount)
-        {
-            this.enabled = false;
-        }
-    }
+	public void LimitSpawning()
+	{
+		if (currentSpawnCount >= maxSpawnCount)
+		{
+			this.enabled = false;
+		}
+	}
 }
